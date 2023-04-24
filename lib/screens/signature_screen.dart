492c4ca2aa9186f97465e871bef9af7f2edff685
MAC OS 12 <nandanig.itpath@gmail.com>
2023-04-24 15:32:15 +0530

    DONE:  - [X] Upload signature image and signature screen.  - [X] Fixed skill module bug.  - [X] Fixed issues in the signature module.  - [X] Completed signature module

--- lib/screens/signature_screen.dart
+++ lib/screens/signature_screen.dart
@@ -3,16 +3,13 @@ import 'dart:ui' as ui;
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:signature/signature.dart';
 
-/**
- * Created by Parth Sheth.
- * Created on 19/04/23 at 4:00 pm
- */
+/// Created by Parth Sheth.
+/// Created on 19/04/23 at 4:00 pm
 
 class SignatureScreen extends StatefulWidget {
   const SignatureScreen({Key? key}) : super(key: key);
@@ -22,50 +19,46 @@ class SignatureScreen extends StatefulWidget {
 }
 
 class _SignatureScreenState extends State<SignatureScreen> {
+  final GlobalKey<SignatureState> _signatureKey = GlobalKey<SignatureState>();
+
   final SignatureController controller = SignatureController(
     penStrokeWidth: 5,
     penColor: Colors.black,
     exportBackgroundColor: Colors.white,
   );
-  Uint8List? _signatureData;
+  final storage = FirebaseStorage.instance;
 
 
-
-  Future<void> submitImage(File image) async {
+  Future<void> saveFile(File image) async {
     if (await Permission.storage.request().isGranted) {
       final signatureData = await controller.toImage();
 
       // Encode ui.Image to PNG
-      final bytes =
-          await signatureData?.toByteData(format: ui.ImageByteFormat.png);
+      final bytes = await signatureData?.toByteData(format: ui.ImageByteFormat.png);
       final buffer = bytes?.buffer;
 
       await image.writeAsBytes(buffer!.asUint8List());
-
-      FirebaseStorage storage = FirebaseStorage.instance;
-      Reference ref = storage.ref().child('signatures/mysignature.png');
-      TaskSnapshot taskSnapshot = await ref.putFile(image);
-
-      String downloadUrl = await taskSnapshot.ref.getDownloadURL();
-
-      // Save image to gallery
       final result = await ImageGallerySaver.saveImage(buffer.asUint8List());
 
       if (result['isSuccess']) {
+        // ignore: use_build_context_synchronously
         ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Signature saved to gallery')));
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to save signature')));
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Failed to save signature')));
       }
 
       setState(() {
-        _signatureData = buffer.asUint8List();
       });
     } else {
+      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Storage permission not granted')));
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -76,38 +69,55 @@ class _SignatureScreenState extends State<SignatureScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
+          SizedBox(
             width: 250,
             height: 200,
-            child: Signature(controller: controller),
+            child: Signature(
+              key: _signatureKey,
+                controller: controller
+            ),
           ),
           const SizedBox(
             height: 20,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Wrap(
+            spacing: 10,
             children: [
               ElevatedButton.icon(
                   onPressed: () async {
-                    final directory = await getExternalStorageDirectory();
-                    final signatureFile =
-                        File('${directory!.path}/signature.png');
-                    await submitImage(signatureFile);
+                    // final directory = await getExternalStorageDirectory();
+                    // final signatureFile = File('/signature.png');
+                    final signatureRef = storage.ref().child('signatures/signature.png');
+                    final signature = await controller.toImage();
+                    final bytes = await signature?.toByteData(format: ui.ImageByteFormat.png);
+                    final file = bytes!.buffer.asUint8List();
+                    await signatureRef.putData(file).whenComplete(() => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('File uploaded successfully'))));
+                    
+                    // await submitImage(File('/signature.png'));
                   },
-                  icon: Icon(Icons.upload),
+                  icon: const Icon(Icons.upload),
                   label: const Text('Upload')),
               ElevatedButton.icon(
-                  onPressed: () {
-                    controller.clear();
+                  onPressed: () async {
+                    final directory = await getExternalStorageDirectory();
+                    final signatureFile = File('${directory!.path}/signature.png');
+                    saveFile(signatureFile);
                   },
-                  icon: Icon(Icons.save),
+                  icon: const Icon(Icons.save),
                   label: const Text('Save to gallery')),
               ElevatedButton.icon(
+                  style: const ButtonStyle(
+                      iconColor: MaterialStatePropertyAll(Colors.red),
+                      textStyle: MaterialStatePropertyAll(
+                          TextStyle(decorationColor: Colors.red))),
                   onPressed: () {
                     controller.clear();
                   },
-                  icon: Icon(Icons.delete),
-                  label: const Text('Remove')),
+                  icon: const Icon(Icons.delete),
+                  label: const Text(
+                    'Remove',
+                    style: TextStyle(color: Colors.red),
+                  )),
             ],
           )
         ],
